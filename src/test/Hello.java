0f78a9cf6e0280d8683714656e655a1418575650
dineshkanna01 <dinesh.kanna@igtsolution.com>
2021-10-05 14:57:41 +0530

    Test

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -6,8 +6,19 @@ public class Hello {
 		System.out.println("dk Pass");
 	}
 	
+	public static void dk1() {
+		System.out.println("dinesh Pass");
+	}
+	
+	public static void dk2() {
+		System.out.println("kanna Pass");
+	}
+	
+	
 	public static void main(String[] args) {
 		dk();
+		dk1();
+		dk2();
 	}
 
 }
